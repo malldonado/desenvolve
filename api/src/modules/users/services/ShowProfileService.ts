8cb1b87f5706a15c017e3dff247c97f3a46138b1
malldonado <matheussilvacontat@gmail.com>
2024-08-22 12:22:15 -0300

    update project

--- api/src/modules/users/services/ShowProfileService.ts
+++ api/src/modules/users/services/ShowProfileService.ts
@@ -1,20 +1,24 @@
+import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
-import AppError from '@shared/errors/AppError';
 
 interface IRequest {
   user_id: string;
 }
 
-class ShowUserService {
+class ShowProfileService {
   public async execute({ user_id }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
+
     const user = await usersRepository.findById(user_id);
 
-    if (!user) throw new AppError('User not found.');
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
     return user;
   }
 }
 
-export default ShowUserService;
+export default ShowProfileService;
--- api/src/modules/users/services/UpdateProfileService.ts
+++ api/src/modules/users/services/UpdateProfileService.ts
@@ -1,21 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { compare, hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
-import AppError from '@shared/errors/AppError';
-import { compare, hash } from 'bcryptjs';
 
 interface IRequest {
   user_id: string;
   name: string;
+  username: string;
   email: string;
-  password: string;
-  old_password: string;
+  password?: string;
+  old_password?: string;
 }
 
 class UpdateProfileService {
   public async execute({
     user_id,
     name,
+    username,
     email,
     password,
     old_password,
@@ -23,19 +25,37 @@ class UpdateProfileService {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findById(user_id);
     if (!user) throw new AppError('User not found.');
+
     const userUpdateEmail = await usersRepository.findByEmail(email);
-    if (userUpdateEmail && userUpdateEmail.id !== user_id)
+    if (userUpdateEmail && userUpdateEmail.id !== user_id) {
       throw new AppError('There is already one user with this email.');
-    if (password && !old_password)
+    }
+
+    const userUpdateUsername = await usersRepository.findByUsername(username);
+    if (userUpdateUsername && userUpdateUsername.id !== user_id) {
+      throw new AppError('There is already one user with this username.');
+    }
+
+    if (password && !old_password) {
       throw new AppError('Old password is required.');
+    }
+
     if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password);
-      if (!checkOldPassword) throw new AppError('Old password does not match.');
+
+      if (!checkOldPassword) {
+        throw new AppError('Old password does not match.');
+      }
+
+      user.password = await hash(password, 8);
     }
-    if (password) user.password = await hash(password, 8);
+
     user.name = name;
     user.email = email;
+    user.username = username;
+
     await usersRepository.save(user);
+
     return user;
   }
 }
